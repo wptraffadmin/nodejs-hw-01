ca6ef1451b628c2afb054ca4e01adbece7d9b4c6
webgalacticdev <webgalacticdev@proton.me>
2025-04-23 12:44:41 +0300

    first commit

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,13 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
 
-console.log(await countContacts());
+export const countContacts = async () => {
+    try {
+        const contacts = await readContacts();
+        return contacts.length;
+      } catch (error) {
+        console.error('Error counting contacts:', error);
+        throw error;
+      }
+};
+
+console.log(`Контактів у файлі: ${await countContacts()}`);
